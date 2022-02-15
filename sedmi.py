df68c5e27cb3e430e5b7415864264c9b94bf5a67
mdrazen27 <84288140+mdrazen27@users.noreply.github.com>
2022-02-15 18:01:15 +0100

    sedmi zadatak

---
+++ sedmi.py
@@ -0,0 +1,15 @@
+def prestupna_godina(godina):
+    """
+    Napisati kod koji za datu godinu odreñuje da li je prestupna i štampa odgovarajuću
+    poruku
+    """
+    if godina%4==0:
+        if godina%100==0 and godina%400!=0:
+            print(godina, " nije prestupna godina")
+        else:
+            print(godina," je prestupna godina")
+    else:
+        print(godina, " nije prestupna godina")
+
+
+prestupna_godina(int(input("Unesite godinu ")))
